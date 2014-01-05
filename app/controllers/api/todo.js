fb9a2f53c5fd0b896138a452d39807b6eddfa5cf
Derice <d3ric3@gmail.com>
2014-01-05 22:42:24 +0800

    added todo api

--- app/controllers/api/todo.js
+++ app/controllers/api/todo.js
@@ -1,3 +1,48 @@
+var _ = require('underscore');
+
 module.exports = function(app) {
+
+	app.get('/api/todo', function(req, res) {
+		global.db.todo.find({}, function(err, todos) {
+			if(err) res.status(500).json(err);
+			res.status(200).json(todos);
+		});
+	});
+
+	app.get('/api/:todoId/todo', function(req, res) {
+		global.db.todo.findOne({ _id: req.params.todoId }, function(err, todo) {
+			if(err) res.status(500).json(err);
+			res.status(200).json(todo);
+		});
+	});
+
+	app.post('/api/todo', function(req, res) {
+		var todo = new global.db.todo(req.body);
+		todo.save(function(err) {
+			if(err) res.status(500).json(err);
+			res.status(200).json(todo);
+		});
+	});
+
+	app.put('/api/:todoId/todo', function(req, res) {
+		global.db.todo.findOne({ _id: req.params.todoId }, function(err, todo) {
+			if(err) res.status(500).json(err);
+			todo = _.extend(todo, req.body);
+			todo.save(function(err) {
+				if(err) res.status(500).json(err);
+				res.status(200).json(todo);
+			});
+		});
+	});
+
+	app.delete('/api/:todoId/todo', function(req, res) {
+		global.db.todo.findOne({ _id: req.params.todoId }, function(err, todo) {
+			if(err) res.status(500).json(err);
+			todo.remove(function(err) {
+				if(err) res.status(500).json(err);
+				res.status(200).json({});
+			});
+		});
+	});
 
 }
